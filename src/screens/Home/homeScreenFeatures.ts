380003348db5ac2e3f036df2683b89967e6d8c5c
jenil-desai <jenildev91@gmail.com>
2024-11-12 00:31:18 +0530

    v2.1.0
    - Built Full On-Boarding Screen
     - 4 Screen Of Application Features
     - 1 Screen Of User Details
      - First Name
      - Last Name
    - Added User Details Into Application Sidebar (Bottom)

---
+++ src/screens/Home/homeScreenFeatures.ts
@@ -0,0 +1,19 @@
+import {Brain, Lightbulb, Rocket} from "lucide-react";
+
+export const homeScreenFeatures = [
+    {
+        icon: Brain,
+        title: "Capture Every Thought",
+        description: "Never let a brilliant idea slip away. Jot down your thoughts anytime, anywhere.",
+    },
+    {
+        icon: Lightbulb,
+        title: "Spark Your Creativity",
+        description: "Organize and connect your ideas to ignite innovation and inspiration.",
+    },
+    {
+        icon: Rocket,
+        title: "Elevate Your Productivity",
+        description: "Transform your notes into action. Boost your efficiency and achieve your goals.",
+    },
+]
